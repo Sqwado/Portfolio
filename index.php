9d5580d9f6945b3605c663031be658bc5d7dfb4e
Sqwado <mateo.luque@ynov.com>
2023-12-05 04:44:37 +0100

    custom html input

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+spl_autoload_register(function ($class) {
+    require __DIR__ . "/src/$class.php";
+});
+
+$parts = explode("/", $_SERVER["REQUEST_URI"]);
+
+// gestion des redirections
+
+if ($parts[1] == "home") {
+    include("page/home.php");
+} elseif ($parts[1] == "loginadmin") {
+    include("page/loginadmin.php");
+} elseif ($parts[1] == "logoutadmin") {
+    include("page/logoutadmin.php");
+} elseif ($parts[1] == "admin") {
+    include("page/admin.php");
+} else {
+    header("Location: /home");
+}
